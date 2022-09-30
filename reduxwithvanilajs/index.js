31faea2200310e2bd0fc526e533b8dc8fc4b9df4
mahan <m.mahmodi1380@gmail.com>
2022-09-30 18:21:16 +0330

    store and its functions.

--- reduxwithvanilajs/index.js
+++ reduxwithvanilajs/index.js
@@ -1,3 +1,8 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const log = console.log; // for logging easier
+
 const BUY_CAKE = 'BUY_CAKE';
 
 function buyCake() {
@@ -13,8 +18,16 @@ const initialState = { numOfCakes: 10, shampoo: 4 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
-      return { ...state, numOfCakes: numOfCakes - 1 };
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
     default:
       return state;
   }
 };
+
+const store = createStore(reducer);
+// log(store);
+log('initial state : ', store.getState());
+
+store.subscribe(() => log('updated state : ', store.getState())); // subscribe gets a function that
+// and execute it whenever a state gets changed
+store.dispatch(buyCake());
