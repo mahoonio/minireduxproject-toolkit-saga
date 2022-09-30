c4cfb64c569b080d0fcd50fb747dfa9ec3be6b0f
mahan <m.mahmodi1380@gmail.com>
2022-10-01 03:24:42 +0330

    seperate reducers and states

--- reduxwithvanilajs/index.js
+++ reduxwithvanilajs/index.js
@@ -5,20 +5,37 @@ const log = console.log; // for logging easier
 
 const BUY_CAKE = 'BUY_CAKE';
 
+const BUY_ICECREAM = 'BUY_ICECREAM';
+
 function buyCake() {
   // action creator
   return { type: BUY_CAKE };
 }
 
+function buyIceCream() {
+  // action creator
+  return { type: BUY_ICECREAM };
+}
+
 //reducer: how to transform state
 // (state , action) => new state
 
-const initialState = { numOfCakes: 10, shampoo: 4 };
+const initialCakeState = { numOfCakes: 10 };
+const initialIceCreamState = { numOfIceCreams: 20 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return { ...state, numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return { ...state, numOfIceCreams: state.numOfIceCreams - 1 };
     default:
       return state;
   }
@@ -37,4 +54,7 @@ store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+
 unsubscribe();
