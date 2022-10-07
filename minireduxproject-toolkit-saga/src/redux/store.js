52c9560657ca7bfadef9974133ed45db53805af1
mahan <m.mahmodi1380@gmail.com>
2022-10-07 06:05:52 +0330

    redux devtools

--- minireduxproject-toolkit-saga/src/redux/store.js
+++ minireduxproject-toolkit-saga/src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
-const store = createStore(rootReducer, applyMiddleware(logger));
+import { composeWithDevTools } from 'redux-devtools-extension';
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
