6b9ce91ab0c9197c399dd3303a6524c93faa4af7
mahan <m.mahmodi1380@gmail.com>
2022-10-07 05:48:19 +0330

    adding redux-logger

--- minireduxproject-toolkit-saga/src/redux/store.js
+++ minireduxproject-toolkit-saga/src/redux/store.js
@@ -1,6 +1,6 @@
-import { combineReducers, createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
-
-const store = createStore(rootReducer);
+import logger from 'redux-logger';
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
